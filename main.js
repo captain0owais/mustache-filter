39b2d1823623e9250914d78a868b865b92daa8b6
Owais <87132814+captain0owais@users.noreply.github.com>
2022-02-13 13:27:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){}
+
+function setup(){
+    canvas = createCanvas(400 , 350);
+    canvas.position(400 , 200);
+}
+
+function draw(){}
+
+function takeSnapshot(){
+    save("filter-image.png");
+}
