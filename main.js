1b20573cae5ef478600ada20e32135ac08c2c60a
Owais <87132814+captain0owais@users.noreply.github.com>
2022-02-17 13:00:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,31 @@ function preload(){}
 
 function setup(){
     canvas = createCanvas(400 , 350);
-    canvas.position(400 , 200);
+    canvas.center();
+    video = createCapture(VIDEO)
+    video.size(400 , 350);
+    video.hide()
+    poseNet = ml5.poseNet(video , modelLoded);
+    poseNet.on("pose" , gotPoses);    
 }
 
-function draw(){}
+function modelLoded(){
+    console.log("poseNet INITIALIZED");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results)
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+        console.log("⚡check⚡")
+        
+    }
+}
+
+function draw(){
+    image(video , 0 , 0 , 400 , 350);
+}
 
 function takeSnapshot(){
     save("filter-image.png");
